16d4f2c16271065bcc8e0d73f3d437ae20c27d7b
KomTudo <129621963+KomTudo@users.noreply.github.com>
2024-02-22 17:39:03 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,11 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
 /*Variables excusas*/
 
 window.onload = () => {
